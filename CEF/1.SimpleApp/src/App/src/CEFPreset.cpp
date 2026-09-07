d36f49f750aab081a1039bf0e092ff78456ca196
Dir-A <g2576612205@gmail.com>
2026-09-07 19:06:02 +0800

    fix: macos

--- CEF/1.SimpleApp/src/App/src/CEFPreset.cpp
+++ CEF/1.SimpleApp/src/App/src/CEFPreset.cpp
@@ -1,14 +1,15 @@
 #include "CEFPreset.hpp"
 
 #ifdef _WIN32
-#include <windows.h>
-#include <filesystem>
+#  include <windows.h>
+#  include <filesystem>
 #elif __linux__
-#include <stdexcept>
-#include <filesystem>
+#  include <stdexcept>
+#  include <filesystem>
 #elif __APPLE__
-#include <filesystem>
-#include "include/wrapper/cef_library_loader.h"
+#  include <filesystem>
+#  include <stdexcept>
+#  include "include/wrapper/cef_library_loader.h"
 auto CEFMacOSEntryInit() -> void;
 auto CEFMacOSEntryClean() -> void;
 #endif
@@ -32,15 +33,14 @@ CEFPreset::CEFPreset([[maybe_unused]] const int argc, [[maybe_unused]] char** ar
   this->args = CefMainArgs{ argc, argv };
   CefString(&this->settings.browser_subprocess_path) = (std::filesystem::current_path() / "CEFRuntime/CEFRuntime").c_str();
 #elif __APPLE__
-  const auto current_path = std::filesystem::path{ argv[0] }.parent_path();
-  const auto framework_dir = current_path / "CEFRuntime/Chromium Embedded Framework.framework/";
-  const auto subprocess_path = current_path / "CEFRuntime/CEFRuntime Helper.app/Contents/MacOS/CEFRuntime Helper";
+  const auto current_path = std::filesystem::path{ argv[0] }.parent_path().parent_path();
+  const auto framework_dir = current_path / "Frameworks/Chromium Embedded Framework.framework";
+  const auto subprocess_path = current_path / "Frameworks/CEFRuntime Helper.app/Contents/MacOS/CEFRuntime Helper";
   if (cef_load_library((framework_dir / "Chromium Embedded Framework").c_str()) != 1)
   {
-    throw std::runtime_error("error");
+    throw std::runtime_error("failed to load the CEF framework");
   }
   this->args = CefMainArgs{ argc, argv };
-  CefString(&this->settings.framework_dir_path) = framework_dir;
   CefString(&this->settings.browser_subprocess_path) = subprocess_path;
   ::CEFMacOSEntryInit();
 #endif
